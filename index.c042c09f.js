73aee004cd6e6781d8cf0bec0082fccdd390b1fc
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2024-01-07 15:27:03 +0000

    Deploying to gh-pages from @ SGlinkau/slawomir@2131046cb2dbb05dc2c0d8c102793322457cdfee 🚀

---
+++ index.c042c09f.js
@@ -0,0 +1,2 @@
+document.getElementById("themeButton").onclick=function(){var t=document.getElementsByTagName("html")[0];"dark"===t.getAttribute("data-bs-theme")?t.setAttribute("data-bs-theme","light"):t.setAttribute("data-bs-theme","dark")};
+//# sourceMappingURL=index.c042c09f.js.map
